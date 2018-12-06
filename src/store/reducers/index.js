2b1c9ef7e0fd1c42fb6e70ead4f3cdb06769b07e
z80 <muzo0@yandex.ru>
2018-12-06 16:31:15 +0300

    fix min preload img height

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -3,33 +3,42 @@
 import * as types from '../actionTypes';
 
 declare interface initialState {
-    posts: mixed[];
-    currentPage: number;
+  posts: mixed[];
+  currentPage: number;
 }
 
 const loadingPosts = [1, 2, 3, 4, 5, 6].map(id => ({
-    id,
-    isLoading: true
+  id,
+  isLoading: true
 }))
 
 const initialState = {
-    posts: [],
-    currentPage: 1
+  posts: [],
+  currentPage: 1
 };
 
+const setDefaultImageHeight = (text: string) => text.replace(/<img /gmi, img => `${img} height="${window.screen.width / 6}px" `)
+
 export default function reduce(state = initialState, action = {}) {
-    switch (action.type) {
-        case types.SET_POSTS:
-            return {
-                ...state,
-                ...action.payload
-            };
-        case types.SET_POSTS_LOADER:
-            return {
-                ...state,
-                posts: loadingPosts
-            }
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case types.SET_POSTS:
+      return {
+        ...state,
+        ...action.payload,
+        posts: action.payload.posts.map(({
+          description,
+          ...post
+        }) => ({
+          ...post,
+          description: setDefaultImageHeight(description || '')
+        }))
+      };
+    case types.SET_POSTS_LOADER:
+      return {
+        ...state,
+        posts: loadingPosts
+      }
+    default:
+      return state;
+  }
 }
